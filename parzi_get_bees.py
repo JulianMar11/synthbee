27e1c611e52a03311cdedd1209caa0a714bde260
Parzival <parzival@trio.li>
2018-05-30 13:54:27 +0200

    einfachereres Dateiformat: Jetzt einfach nur pfad zu ordner mit mp4 videos angeben.  darin eine setting.txt datei einfügen wo der bildauschnitt definiert wird, der analysiert werden soll. besonders nice für remote rechner...

--- parzi_get_bees.py
+++ parzi_get_bees.py
@@ -5,16 +5,37 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import time
-import PATH
 import util
+import re
+import sys
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
-def multiprocessing(func, frames, frame_indices, prod_run, proc_dir, workers):
-    with ProcessPoolExecutor(max_workers=workers) as executor:
-        res = executor.map(func, frames, frame_indices, prod_run, proc_dir  )
+def multiprocessing(func, frames, frame_indices, sub_frames, prod_run, data_dir, output_dir, bg_img_path, num_cpu):
+    """ Makes use of multi kernels. """
+    with ProcessPoolExecutor(max_workers=num_cpu) as executor:
+        res = executor.map(func, frames, frame_indices, sub_frames, prod_run, data_dir, output_dir, bg_img_path  )
     return list(res)
 
-<<<<<<< HEAD
+def load_subframe_metadata(path):
+    """Opens settings.txt and loads how the subframes of a given video bucket are specified."""
+    try:
+        with open(path) as inputFileHandle:
+            result = []
+            content = inputFileHandle.read() 
+            content = content.split("\n")[0]
+            subframes = content.split("/")
+            for subframe in subframes:
+                print(subframe)
+                result.append(subframe.split(","))
+
+            print("This is the subframe metadata")
+            print(result)
+            return result
+    except IOError:
+        sys.stderr.write( "[myScript] - Error: Could not open %s\n" % (path) )
+        sys.exit(-1)
+
+
 
 # PATHES!
 # cwd: DATA
@@ -26,78 +47,61 @@ def multiprocessing(func, frames, frame_indices, prod_run, proc_dir, workers):
 num_cpu = 3
 # no print commands if true
 production_run = True
-# first store all data in RAM
-save_array = []
-=======
-print(PATH.DATAPATH)
-
-## Settings
-# scale images for processing (Nevertheless all imgs are stored in best quality)
-scale = 1
-# Size of the dilation "cursor"
-dilateRectDim = int(np.floor(scale*scale*24))
-# Path to bee directory
-beePath = 'bees/' + str(datetime.datetime.now()) + '/'
-# Blops smaller than this will be removed
-minBlopSize = int(4000 * scale * scale)
-# A Bee is supposed to be this big. Used to calculate number of clusters
-beeBlopSize = int(8000 * scale * scale)
-# Threshold for differntiation (low if noise is low, high otherwise)
-diff_threshold = 25
-# True if imgs should be saved with more than one bee
-saveCollidingBoundingBoxes = True
-# True if bee imgs should be saved
-saveBees = True
-# Skips frames
-start_frame = 0
-# Last frame
-end_frame = 0
->>>>>>> 088ed3d8cf8663d4ac6803af95ab606b865bb2ce
-
-## Setup
-# Create the specified directory
-os.chdir(PATH.DATAPATH)
-# Path to folder where each process stores its data
-process_dir =  'bees/' + "multicore_" + str(datetime.datetime.now()) + '/'
-os.mkdir(process_dir)
-os.chdir(process_dir)
-beePath = ""
-os.mkdir(beePath + 'single/')
-os.mkdir(beePath + 'multi/')
-os.mkdir(beePath + 'dump/')
-os.mkdir(beePath + 'single/bee/')
-os.mkdir(beePath + 'single/mask/')
-os.mkdir(beePath + 'single/overlay/')
-os.mkdir(beePath + 'multi/bee/')
-os.mkdir(beePath + 'multi/mask/')
-os.mkdir(beePath + 'multi/overlay/')
-os.mkdir(beePath + 'dump/multi/')
-os.mkdir(beePath + 'dump/single/')
-os.mkdir(beePath + 'dump/multi/bee')
-os.mkdir(beePath + 'dump/single/bee')
-os.mkdir(beePath + 'dump/single/overlay')
-os.mkdir(beePath + 'dump/multi/mask')
-os.mkdir(beePath + 'dump/single/mask')
-os.mkdir(beePath + 'dump/multi/overlay')
-os.chdir("../../")
-
+## Data bucket
+# video files *.mp4 and settings.txt
+data_dir = "/home/bemootzer/Dokumente/SoftwareProjekte/Bienen/DATA"
+video_bucket = "videos_charge3" # relative to DATA/
+bg_img_path = data_dir + "/" + video_bucket + "/background.png"
+settings_path = data_dir + "/" + video_bucket + "/settings.txt"
 # Skip frames until start frame
 start_frame = 10
 # End at given frame
 end_frame = 0
 
-videos = os.listdir('videos/')
-video_index = 1
 
+## Setup
+# Create the specified directory. Path to folder where each process stores its data
+output_dir = data_dir + '/bees/' + video_bucket + "_" + str(datetime.datetime.now()) + '/'
+os.mkdir(output_dir)
+os.chdir(output_dir)
+os.mkdir('single/')
+os.mkdir('multi/')
+os.mkdir('dump/')
+os.mkdir('single/bee/')
+os.mkdir('single/mask/')
+os.mkdir('single/overlay/')
+os.mkdir('multi/bee/')
+os.mkdir('multi/mask/')
+os.mkdir('multi/overlay/')
+os.mkdir('dump/multi/')
+os.mkdir('dump/single/')
+os.mkdir('dump/multi/bee')
+os.mkdir('dump/single/bee')
+os.mkdir('dump/single/overlay')
+os.mkdir('dump/multi/mask')
+os.mkdir('dump/single/mask')
+os.mkdir('dump/multi/overlay')
+os.chdir(data_dir)
+
+# load sub_frame sizes
+subframes = load_subframe_metadata(settings_path)
+
+videos = os.listdir(video_bucket)
+video_index = 0
+pattern = re.compile("^.*mp4$")
 for video in videos:    
+    # only process .mp4 files
+    if not pattern.match(video):
+        continue
+    video_index = video_index + 1
     frames = []
     frame_indices = []
+    proc_subframes = []
     # loads video file
-    print(video)
-    cap = cv2.VideoCapture('videos/'  + video)
+    cap = cv2.VideoCapture(data_dir + "/" + video_bucket + "/"  + video)
+    # number of frames in video file
     max_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    print("New video with " + str(max_frames) + ' frames')
-    print("working directory: " + os.getcwd())
+    # last frame index (can be used to skip last frames)
     end_frame = int(max_frames)
     # skips frames until defined start_frame
     cap.set(1, start_frame)
@@ -105,18 +109,23 @@ for video in videos:
     for frame_index in range(0, end_frame - start_frame):
         # add all frames from one clip as arguments for pool
         _, frame = cap.read()
-        frames.append(frame)
-        frame_indices.append(str(video_index) + "_" + str(frame_index))
-    video_index = video_index + 1
 
-    ## Now there is a list of all frames of all videos
-    # def calc(frame, frame_index, production_run, process_dir):
+        subframe_index = 0
+        for subframe in subframes:
+            subframe_index = subframe_index + 1
+     
+            frames.append(frame)
+            frame_indices.append(str(video_index) + "_" + str(subframe_index) + "_" + str(frame_index))
+            proc_subframes.append(subframe)
+
+    ## Now there is a list of all frames of current video
     N = len(frames)
-    print("amount of frames to be processed: " + str(N))
-    prod_run = [production_run for i in range(N) ]
-    proc_dir = [process_dir for i in range(N) ]
+    ## blows up constants to hand over to processes
+    proc_prod_run = [production_run for i in range(N) ]
+    proc_output_dir = [output_dir for i in range(N) ]
+    proc_bg_img_path = [bg_img_path for i in range(N) ]
+    proc_data_dir = [data_dir for i in range(N) ]
 
-
-    print("start of video number " + str(video_index) + "(" + video + ") " + " of " + str(len(videos)))
-    multiprocessing(util.calc, frames, frame_indices, prod_run, proc_dir, num_cpu )
+    print("start of video number " + str(video_index) + " - " + str(max_frames) + " (" + video + ") " + " of " + str(len(videos)))
+    multiprocessing(util.calc, frames, frame_indices, proc_subframes, proc_prod_run, proc_data_dir, proc_output_dir, proc_bg_img_path, num_cpu )
 
