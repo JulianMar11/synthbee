de449507b03d37d7a16d33c4b21af015bea5b7fe
JulianMar11 <julian.marstaller@online.de>
2018-06-30 22:29:26 +0000

    newpath

--- PATH.py
+++ PATH.py
@@ -1,5 +1,5 @@
 import os
 
 # Path to data depending of USER variable
-DATAPATH = '/home/Julian/' if os.getenv('USER') == 'bemootzer' else ( '/home/ubuntu/DATA/' if os.get('USER') == 'ubuntu' else '/Users/Julian/Desktop/Dropbox/synthbeedata/')
-
+DATAPATH = '/home/Julian/' if os.getenv('USER') == 'bemootzer' else ( '/home/ubuntu/DATA/' if os.getenv('USER') == 'ubuntu' else '/home/Julian/')
+    
